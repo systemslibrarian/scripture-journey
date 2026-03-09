cbcc2ee08a8fb39566fd5f5673bf65528e421dee
Paul Clark <systemslibrarian@gmail.com>
2026-03-09 18:06:25 +0000

    Pin Prisma to v6, fix client config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,1 @@
-import path from 'node:path'
-import { defineConfig } from 'prisma/config'
 
-export default defineConfig({
-  earlyAccess: true,
-  schema: path.join(__dirname, 'prisma', 'schema.prisma'),
-  migrate: {
-    async development() {
-      return {
-        url: process.env.DATABASE_URL!,
-      }
-    },
-  },
-})
